272e7aaf737ae684a2d60176e1c0cf94fa73a057
Harshit Mody <55501384+harshitmody72@users.noreply.github.com>
2020-06-21 11:24:09 +0530

    Add files via upload

---
+++ C++ Program/Basic_Calculator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<string>
+
+using namespace std;
+
+
+int main()
+{
+    int a , b , sum ;
+
+    cout << "Enter a Number\n";
+    cin >> a;
+
+    cout << "Enter a another number\n";
+    cin >> b;
+
+    sum = a + b;
+
+    cout << "The sum those number's is " << sum << endl;
+
+    return 0;
+}
