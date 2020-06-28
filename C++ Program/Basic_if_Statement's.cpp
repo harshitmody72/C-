5fa3595945c1199b713f25b075e594a2df9da135
Harshit Mody <55501384+harshitmody72@users.noreply.github.com>
2020-06-28 09:51:50 +0530

    Add files via upload

---
+++ C++ Program/Basic_if_Statement's.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<string>
+
+using namespace std;
+
+
+int main()
+{
+  // true is boolean value
+
+    if (true) {
+        cout<< "hii"<<endl;
+    }
+
+//Both are same only when we have only one line of code we just right it after the condition and there is no need of Curly bracket's
+
+    if (true) cout << "hii"<<endl;
+
+    return 0;
+}
